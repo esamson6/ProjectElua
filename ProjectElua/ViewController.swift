bc92799bc9d78d58b79ebe24d05864b880402a98
esamson6 <erin.samson34@yahoo.com>
2021-03-28 09:28:34 -1000

    Commit About Me VC

--- ProjectElua/ViewController.swift
+++ ProjectElua/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myNameLabel: UILabel!
+    @IBOutlet weak var erinLabel: UILabel!
+    @IBOutlet weak var myMajorLabel: UILabel!
+    @IBOutlet weak var majorLabel: UILabel!    
+    @IBOutlet weak var springClassesLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
